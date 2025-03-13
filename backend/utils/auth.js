d07d54773b9cd1a56db9e4d2e11fe2ca98ada04c
AmirJT <ahjt28@gmail.com>
2025-03-12 23:15:42 -0700

    Fixed authentication middleware to allow unauthenticated requests on Render

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -3,6 +3,7 @@ const { AuthenticationError } = require("apollo-server-express");
 require("dotenv").config();
 
 module.exports = {
+
   signToken: (user) => {
     return jwt.sign(
       { id: user._id, email: user.email },
@@ -14,19 +15,18 @@ module.exports = {
   authMiddleware: ({ req }) => {
     let token = req.headers.authorization || "";
 
-    if (token.startsWith("Bearer ")) {
-      token = token.split(" ")[1]; 
-    } else {
-      throw new AuthenticationError("No token provided");
-    }
 
     if (!token) {
-      return req; 
+      return { user: null }; 
+    }
+
+    if (token.startsWith("Bearer ")) {
+      token = token.split(" ")[1];
     }
 
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      return { user: decoded }; 
+      return { user: decoded };   
     } catch (err) {
       throw new AuthenticationError("Invalid or expired token");
     }
